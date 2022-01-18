a7529709fb61ea6221d8c49c5e0722a1caabe993
bo <25973794+bourneo@users.noreply.github.com>
2022-01-18 18:57:16 +0800

    update Batchfile

--- Script/bat/bat结合ffmpeg批量合并视频/bat结合ffmpeg批量合并视频.bat
+++ Script/bat/bat结合ffmpeg批量合并视频/bat结合ffmpeg批量合并视频.bat
@@ -9,6 +9,8 @@ ffmpeg.exe -f concat -safe 0 -i file-list.txt -c copy out.webm
 pause
 
 rem 需要安装 ffmpeg
-rem -safe 0: 防止 Operation not permitted
+rem 文件路径不能包含空格，标点或者特殊符号
 rem *.webm 限定了遍历文件的视频格式
 rem out.webm 指定了输出视频的格式
+rem -safe 0: 防止 Operation not permitted
+
